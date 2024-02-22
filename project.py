d8968be85e215da059cc06e10c575d2359c385f1
zdvinkofff <119503489+zdvinkofff@users.noreply.github.com>
2024-02-22 21:42:57 +0300

    Update project.py

--- project.py
+++ project.py
@@ -1,33 +1,32 @@
 import os
-import json
 import csv
 
-class PriceMachine():
+
+class PriceMachine:
 
     def __init__(self):
         self.data = []
-        self.result = ''
-        self.name_length = 0
 
-    def load_prices(self, file_path=r'C:\Users\Denis\PycharmProjects\pythonProject2', encoding='utf-8'):
+    def load_prices(self, file_path='C:/Users/Denis/PycharmProjects/pythonProject2', encoding='utf-8'):
         for file_name in os.listdir(file_path):
             if "price" in file_name:
-                with open(os.path.join(file_path, file_name), 'r') as file:
+                with open(os.path.join(file_path, file_name), 'r', encoding=encoding) as file:
                     csv_reader = csv.DictReader(file, delimiter=';')
                     for row in csv_reader:
                         self.data.append(row)
 
-    def export_to_html(self, file_path='output.html'):
-        with open(os.path.join(file_path, file_name), 'r',encoding='utf-8'):
-            file.write('<html><body><table border="1">')
+    def export_to_html(self, file_path='C:/Users/Denis/PycharmProjects/pythonProject2/output.html'):
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write('<html><body><table border="1">\n')
             file.write(
-                '<tr><th>№</th><th>Наименование</th><th>цена</th><th>вес</th><th>файл</th><th>цена за кг.</th></tr>')
+                '<tr><th>№</th><th>Наименование</th><th>цена</th><th>вес</th><th>файл</th><th>цена за кг.</th></tr>\n')
             for idx, item in enumerate(self.data, 1):
-                file.write(
-                    f'<tr><td>{idx}</td><td>{item.get("название", "")}</td><td>{item.get("цена", "")}'
-                    f'</td><td>{item.get("вес", "")}</td><td>{item.get("файл", "")}'
-                    f'</td><td>{float(item.get("цена", 0)) / float(item.get("вес", 1))}</td></tr>')
+                file.write(f'<tr><td>{idx}</td><td>{item.get("Наименование", "")}</td>'
+                           f'<td>{item.get("цена", "")}</td><td>{item.get("вес", "")}</td>'
+                           f'<td>{item.get("файл", "")}</td><td>{float(item.get("цена", 0)) / float(item.get("вес", 1))}</td></tr>\n')
             file.write('</table></body></html>')
+
+        print(f'Файл {file_path} успешно создан.')
 
     def find_text(self, text):
         result = []
@@ -38,18 +37,11 @@ class PriceMachine():
         return sorted(result, key=lambda x: float(x.get("цена", 0)) / float(x.get("вес", 1)))
 
 
+
 pm = PriceMachine()
-print(pm.load_prices(r"C:\Users\Denis\PycharmProjects\pythonProject2", encoding='utf-8'))
 
-while True:
-    search_text = input("Введите текст для поиска (для выхода введите 'exit'): ")
-    if search_text.lower() == 'exit':
-        print("Работа завершена.")
-        break
 
-    found_items = pm.find_text(search_text)
-    for idx, item in enumerate(found_items, 1):
-        print(f"{idx} {item.get('название', '')} {item.get('цена', '')} {item.get('вес', '')} {item.get('файл', '')}  "
-              f" {float(item.get('цена', 0)) / float(item.get('вес', 1))}")
-print('the end')
-print(pm.export_to_html(r"C:\Users\Denis\PycharmProjects\pythonProject2"))
+pm.load_prices(r"C:/Users/Denis/PycharmProjects/pythonProject2", encoding='utf-8')
+
+
+pm.export_to_html(r"C:/Users/Denis/PycharmProjects/pythonProject2/output.html")
